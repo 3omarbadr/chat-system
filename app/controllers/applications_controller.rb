cc4e4127211fe6a889a4b60e9cc18205bc245ae3
3omarbadr <omarbadr167@gmail.com>
2024-06-10 10:50:14 +0300

    Add `applications_controller`

---
+++ app/controllers/applications_controller.rb
@@ -0,0 +1,39 @@
+class ApplicationsController < ApplicationController
+    before_action :set_application, only: [:show, :update]
+
+    def index
+        @applications = Application.all
+        render json: @applications
+    end
+
+    def create
+        @application = Application.create(application_params)
+        if @application.save
+          render json: @application, status: :created
+        else
+          render json: @application.errors, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        application = Application.find_by(token: params[:token])
+        application.update(application_params)
+        render json: application
+    end
+
+    def show
+        application = Application.find_by(token: params[:token])
+        render json: application
+    end
+
+    private
+
+    def set_application
+        @application = Application.find_by(token: params[:token])
+        render json: { error: 'Application not found' }, status: :not_found unless @application
+    end
+
+    def application_params
+        params.require(:application).permit(:name)
+    end
+end
